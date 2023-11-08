3e654f26e84d24f950757126ea3ceabbab8d614f
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2023-11-08 11:30:29 +0700

    rdwan

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,42 +5,42 @@ import googleplay from "../../assets/image 11.png"
 import appstore from "../../assets/image 12.png"
 function FooterComponents() {
     return (
-        <div className='w-full bg-red-400 bottom-0 h-[404px] overflow-hidden ' style={{
+        <div className='w-full bg-red-400 bottom-0 h-[253px] overflow-hidden ' style={{
             backgroundImage: `url(${footerbg})`, backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat'
         }}>
             <div className='w-11/12 mx-auto'>
                 <div className=' grid grid-cols-5  '>
-                    <div className='mt-16'>
-                        <img src={RaceLogo} />
+                    <div className='mt-5'>
+                        <img className='w-[88px] h-[57px]' src={RaceLogo} />
                     </div>
-                    <div className='mt-16 text-white'>
-                        <p className='text-[28px]'>Perusahaan</p>
-                        <p>Profil Perusahaan</p>
-                        <p>Hubungan Kami</p>
-                        <p>Karir</p>
-                        <p className='text-[28px] mt-8'>Lainnya</p>
-                        <p>FAQ</p>
-                        <p>Kemitraan</p>
+                    <div className='mt-5 text-white'>
+                        <p className='text-[24px]'>Perusahaan</p>
+                        <p className='text-[16px]'>Profil Perusahaan</p>
+                        <p className='text-[16px]'>Hubungan Kami</p>
+                        <p className='text-[16px]'>Karir</p>
+                        <p className='text-[24px] mt-8'>Lainnya</p>
+                        <p className='text-[16px]'>FAQ</p>
+                        <p className='text-[16px]'>Kemitraan</p>
                     </div>
-                    <div className='mt-16 text-white'>
-                        <p className='text-[28px]'>Produk</p>
-                        <p>Delivery Service</p>
-                        <p>Fulfillment Service</p>
+                    <div className='mt-5 text-white'>
+                        <p className='text-[24px]'>Produk</p>
+                        <p className='text-[16px]'>Delivery Service</p>
+                        <p className='text-[16px]'>Fulfillment Service</p>
                     </div>
-                    <div className='mt-16 text-white'>
-                        <p className='text-[28px]'>Pengiriman</p>
-                        <p>Lacak Resi</p>
-                        <p>Cek Tarif</p>
-                        <p>Lokasi</p>
+                    <div className='mt-5 text-white'>
+                        <p className='text-[24px]'>Pengiriman</p>
+                        <p className='text-[16px]'>Lacak Resi</p>
+                        <p className='text-[16px]'>Cek Tarif</p>
+                        <p className='text-[16px]'>Lokasi</p>
                     </div>
-                    <div className='mt-16 text-white'>
-                        <p className='text-[28px]'>Download Apps</p>
-                        <img className='mt-3' src={googleplay} />
-                        <img className="mt-3" src={appstore} />
+                    <div className='mt-5 text-white'>
+                        <p className='text-[24px]'>Download Apps</p>
+                        <img className='mt-3 w-[150px] h-[48px]' src={googleplay} />
+                        <img className="mt-3 w-[150px] h-[48px]" src={appstore} />
                     </div>
                 </div>
-                <p className='text-center mt-16 text-white font-semibold text-[12px]'>this website was powered by EurekaDev</p>
+                {/* <p className='text-center mt-16 text-white font-semibold text-[12px]'>this website was powered by EurekaDev</p> */}
             </div>
         </div>
     )
