9de4a3dc16cab14d1e6c5db7d761008efbe3d917
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2023-11-08 16:29:57 +0700

    ridwan

--- src/components/CekResi/index.jsx
+++ src/components/CekResi/index.jsx
@@ -3,24 +3,39 @@ import Rectangle from "../../assets/Rectangle 37.png"
 import FooterComponents from '../Footer'
 function CekResiKomponents() {
     return (
-        <div className=' '>
-            <div className='md:w-9/12 mx-auto mt-40  ph:bg-red-500'>
-                <p className="text-[32px] font-semibold font-['Plus_Jakarta_Sans'] bg-clip-text text-transparent" style={{ backgroundImage: 'linear-gradient(92deg, #F05423 11.5%, #A83CCE 46.87%, #3D62B0 85.41%)' }}>
-                    Lacak Paket Anda
-                </p>
-                <div className='md:w-[1064px] h-[105px]flex items-center  mt-5 shadow rounded-lg'>
-                    <input className='w-[874px] h-[60px] m-5 border rounded-lg' placeholder='Masukkan nomor resi pengiriman anda'></input>
-                    <button className='bg-[#F05423] p-3 rounded-md w-[130px] h-[45px] text-white font-semibold '>Search</button>
+        <>
+            <div className='mt-32 w-9/12 mx-auto  '>
+                {/* ini HP */}
+                <div className=' md:hidden mt-32 w-11/12 mx-auto  '>
+                    <img className='' src={Rectangle} />
+                    <p className=" text-center text-[32px] font-semibold font-['Plus_Jakarta_Sans'] bg-clip-text text-transparent" style={{ backgroundImage: 'linear-gradient(92deg, #F05423 11.5%, #A83CCE 46.87%, #3D62B0 85.41%)' }}>
+                        Lacak Paket Anda
+                    </p>
+
+                    <div className='md:w-[1064px] md:h-[105px] ph:h-[130px] flex flex-col  mt-5 shadow rounded-lg'>
+                        <input className='md:w-[874px] md:h-[60px] m-5  border rounded-md' placeholder='Masukkan nomor resi pengiriman anda'></input>
+                        <div className='flex justify-center'>
+                            <button className='bg-[#F05423] ph:w-[260px]  p-3 rounded-md h-[45px] text-white font-semibold '>Search</button>
+                        </div>
+                    </div>
                 </div>
-                <div className='mt-16 justify-center flex'>
-                    <img src={Rectangle} />
+                {/* Ini PC */}
+                <div>
+                    <p className=" text-start text-[32px] font-semibold font-['Plus_Jakarta_Sans'] bg-clip-text text-transparent" style={{ backgroundImage: 'linear-gradient(92deg, #F05423 11.5%, #A83CCE 46.87%, #3D62B0 85.41%)' }}>
+                        Lacak Paket Anda
+                    </p>
+                    <div className='md:w-[1064px] md:h-[105px] ph:h-[130px] mt-5 shadow rounded-lg'>
+                        <input className='md:w-[874px] md:h-[60px] m-5  border rounded-md' placeholder='Masukkan nomor resi pengiriman anda'></input>
+                        <button className='bg-[#F05423] ph:w-[260px]  p-3 rounded-md h-[45px] text-white font-semibold '>Search</button>
+                    </div>
                 </div>
-                <p className='text-center mt-24 text-[#A6A6A6]'>Ada kendala?<span className='text-[#F05423]'>Hubungi kami</span> </p>
+                <div className='justify-center flex mt-32'>
+                    <img className='' src={Rectangle} />
+                </div>
+                <p className='text-center mt-24 mb-24 text-black'>Ada kendala?<span className='text-[#F05423]'>Hubungi kami</span> </p>
             </div>
-            <div className='mt-28'>
-                <FooterComponents />
-            </div>
-        </div>
+            <FooterComponents />
+        </>
     )
 }
 
