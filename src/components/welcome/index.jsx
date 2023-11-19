10792ebc171b85e2d7b75b0a45f3ab3b0a9f05e6
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2023-11-19 09:57:59 +0700

    banner

--- src/components/welcome/index.jsx
+++ src/components/welcome/index.jsx
@@ -2,16 +2,20 @@
 import React from 'react';
 import Background from "../../assets/Background.png";
 import Race_New_Bg from "../../assets/Race_New _Bg.png";
+import Property1 from "../../assets/Property 1=Banner 1.png";
+import Property2 from "../../assets/Property 1=Banner 2.png";
+import Property3 from "../../assets/Property 1=Banner 3.png";
 import StatusbarComponents from '../statusbar';
 import SejarahComponeent from '../Sejarah';
 import { motion } from 'framer-motion';
+import { Carousel } from 'antd';
 function Welcome() {
     return (
         <>
+            <StatusbarComponents />
             <div
-                className='h-screen overflow-hidden ' style={{ backgroundImage: `url(${Background})` }}>
-                <StatusbarComponents />
-                <motion.div
+                className=' overflow-hidden  bg-yellow-200 md:w-screen'>
+                {/* <motion.div
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{
@@ -19,29 +23,29 @@ function Welcome() {
                         delay: 0.5,
                         ease: [0, 0.71, 0.2, 1.01]
                     }}
-                    className='text-white  mt-32 '>
-                    {/* <div className='flex justify-center mb-10'>
+                    className='text-white  mt-32 '> */}
+                {/* <div className='flex justify-center mb-10'>
                         <img className='md:opacity-0 ' src={Race_New_Bg} />
                     </div> */}
-                    <p className='text-[24px] md:text-[48px] text-center'>Welcome to Raja Cepat</p>
-                    <p className=' text-[14px] text-center md:text-[24px] mx-auto  w-9/12 mt-8'>In this site, you can get information about the company profile, service programs provided, <br />check your delivery receipt and careers.<br />
-                        Let’s scroll through this website to get to know more about Raja Cepat!</p>
-                </motion.div>
-                <motion.div
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{
-                        duration: 0.8,
-                        delay: 0.5,
-                        ease: [0, 0.71, 0.2, 1.01]
-                    }}
-                    className='flex justify-center mt-10 '>
-                    <img className='hidden md:block' src={Race_New_Bg} />
-                    <div className='md:hidden mt-16'>
-                    <button className=' border-white border-2 w-[105px] h-[38px] text-white font-semibold text-[14px] rounded-[10px]'>Start Explore</button>
+                <Carousel autoplay className='md:w-screen bg-red-400 flex justify-center md:h-1/6'>
+                    <div className='flex justify-center'>
+                        <img className='w-screen' src={Property1} />
                     </div>
-                </motion.div>
+
+                    <div>
+                        <img className='w-screen' src={Property2} />
+                    </div>
+                    <div>
+                        <img className='w-screen' src={Property3} />
+                    </div>
+
+                </Carousel>
+                {/* </motion.div> */}
             </div>
+                <div className='h-auto bg-red-500 flex items-center justify-between p-12 mx-auto'>
+                    <p> Let’s explore more about Raja Cepat</p>
+                    <button>Explore Now</button>
+                </div>
         </>
     );
 }
